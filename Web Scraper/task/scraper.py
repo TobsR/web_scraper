b15679364030988ea78118f70a9e96437f66d2a2
TobsR <rufio86@gmx.de>
2021-04-05 15:54:47 +0200

    initial commit

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,45 +1,78 @@
 # import json
+import os
 import requests
 from bs4 import BeautifulSoup
 import string
 
+article_types = {'Article': 'c-article-body', 'Book Review': 'article__body cleared',
+                 'Books & Arts': 'article__body cleared', 'Books Received': '',
+                 'Correspondence': 'article__body cleared', 'Editorial': 'article__body cleared',
+                 'Erratum': '', 'Letter': 'c-article-body', 'Matters Arising': '',
+                 'Miscellany': '', 'News': 'article__body cleared', 'News & Views': 'article__body cleared',
+                 'News Feature': 'article__body cleared', 'News in Brief': 'entry-content',
+                 'Obituary': 'article__body cleared', 'Opinion': 'c-article-body',
+                 'Research Highlight': 'article-item__body', 'Review Article': 'c-article-body',
+                 'Scientific Correspondence': 'c-article-section', 'Supplement to Nature': ''}
+
 
 class WebScrapper:
     def __init__(self, url):
-        self.url = url
-        self.request = requests.get(url)
+        self.url = url + '/nature/articles'
+        self.request = requests.get(self.url)
         self.list_articles = []
+        self.pages_akt = 0
+        self.pages_n = int(input('Enter number of pages to search: '))
+        self.article_types = input('Enter the article type: ')
+        if self.article_types not in article_types:
+            exit()
 
     def __str__(self):
         return f"Saved articles: {self.list_articles}"
 
     def get_info(self):
-        if self.request:
-            head_link = "https://www.nature.com"
-            soup = BeautifulSoup(self.request.content, "html.parser")
-            articles = soup.find_all("article")
 
-            for article in articles:
-                article_type = article.find("span", class_="c-meta__type").text
+        global article_types
 
-                if article_type == "News":
-                    tail_link = article.find("a", {"data-track-action": "view article"}).get("href")
+        head_link = "https://www.nature.com"
 
-                    hyperlink = requests.get(head_link + tail_link)
-                    hyperlink_soup = BeautifulSoup(hyperlink.content, "html.parser")
-                    hyperlink_body = hyperlink_soup.find("div", class_="article__body").text.strip()
+        while self.pages_akt < self.pages_n:
+            self.pages_akt += 1
+            if not os.path.isdir(f'Page_{self.pages_akt}'):
+                os.mkdir(f'Page_{self.pages_akt}')
 
-                    translator = str.maketrans('', '', string.punctuation)
-                    title = hyperlink_soup.find("title").text.translate(translator).replace(" ", "_")
+            if self.pages_akt > 1:
+                pages_next = soup.find("li", {"data-page": "next"})
+                self.url = head_link + pages_next.find("a", class_="c-pagination__link").get("href")
+                self.request = requests.get(self.url)
 
-                    self.save_to_file(title, hyperlink_body)
-            return "Content saved."
-        else:
-            return f"The URL returned {self.request.status_code}"
+            if self.request:
+                soup = BeautifulSoup(self.request.content, "html.parser")
+                articles = soup.find_all("article")  # self.article_types
+
+                for article in articles:
+                    article_type = article.find("span", class_="c-meta__type").text
+
+                    if article_type == self.article_types:
+                        tail_link = article.find("a", {"data-track-action": "view article"}).get("href")
+
+                        hyperlink = requests.get(head_link + tail_link)
+                        hyperlink_soup = BeautifulSoup(hyperlink.content, "html.parser")
+                        hyperlink_body = hyperlink_soup.find("div", class_=article_types[self.article_types]).text.strip()
+
+                        translator = str.maketrans('', '', string.punctuation)
+                        # test = hyperlink_soup.find("title").text
+                        # test2 = hyperlink_soup.find("meta", {"property": "og:title"})['content']
+                        title = hyperlink_soup.find("meta", {"property": "og:title"})['content'].translate(translator).replace(" ", "_")
+
+                        self.save_to_file(title, hyperlink_body)
+
+            else:
+                return f"The URL returned {self.request.status_code}"
+        return "Saved all articles."
 
     def save_to_file(self, filename, hyperlink_body):
         try:
-            with open(f"{filename}.txt", 'w', encoding="UTF-8") as file:
+            with open(f'Page_{self.pages_akt}/{filename}.txt', 'w', encoding="UTF-8") as file:
                 print(hyperlink_body, file=file)
 
             self.list_articles.append(f"{filename}.txt")
@@ -48,6 +81,6 @@ class WebScrapper:
 
 
 if __name__ == '__main__':
-    r = WebScrapper("https://www.nature.com/nature/articles")
+    r = WebScrapper("https://www.nature.com")
     print(r.get_info())
     print(r)
